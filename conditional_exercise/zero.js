429cd54822fd0d8d87ce03bffc70f3251ccc131b
Anya <annagapyuk@gmail.com>
2021-10-11 19:27:48 -0400

    conditionals in progress

---
+++ conditional_exercise/zero.js
@@ -0,0 +1,43 @@
+if (true) {
+  console.log("foo");
+}
+
+if (false) {
+  console.log("bar");
+}
+
+if (false || false) {
+  console.log("boop");
+}
+
+if (true || false) {
+  console.log("beep");
+}
+
+let num = 40;
+if (num > 0) {
+  console.log("zip");
+}
+if (num % 2 === 0) {
+  console.log("zoop");
+}
+
+let word = "jeep";
+if (word[0] === "d") {
+  console.log("yeah");
+} else {
+  console.log("nah");
+}
+
+let sentence = "roger that";
+
+if (sentence[sentence.length - 1] === "t") {
+  console.log("ends in t");
+} else {
+  console.log("does not end in t");
+}
+if (sentence.length <= 4) {
+  console.log("short");
+} else {
+  console.log("long");
+}
