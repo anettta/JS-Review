8f39fc14f9d290a93b8df6f6242d35ef2096908e
Anya <annagapyuk@gmail.com>
2021-10-11 11:50:40 -0400

    updated expressions

--- expressions.js
+++ expressions.js
@@ -1,3 +1,10 @@
-// 14 % 5 = remainder of division
+// numbers
+
+// 14 % 5 = remainder of the division
 // 14 mod 5
-console.log(14 % 5);
+// console.log(14 % 5); // 4
+// console.log(4 % 10); // 0r4 = 4
+
+// strings
+// .toLowerCase()
+// .toUpperCase()
