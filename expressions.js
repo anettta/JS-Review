505b7645300b9da40b3f77a5758fc1dfd45aa564
Anya <annagapyuk@gmail.com>
2021-10-11 14:10:17 -0400

    added comparisons

--- expressions.js
+++ expressions.js
@@ -6,5 +6,13 @@
 // console.log(4 % 10); // 0r4 = 4
 
 // strings
+
 // .toLowerCase()
 // .toUpperCase()
+
+// booleans
+// &&  has higher order priority over ||
+console.log((false && false) || true);
+// true && true => true all other false
+// false || false => false all other true
+console.log(true && true);
